1d0eadec38eb8ddba0c0e3e696478becffd68127
Sherry Martin <sm674@cornell.edu>
2018-09-30 20:02:13 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,18 +1,21 @@
 class Artist
-
   attr_accessor :name
   attr_reader :songs
-  
+
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
   include Memorable::InstanceMethods
   include Paramable::InstanceMethods
-  
+
   @@artists = []
 
   def initialize
-    super
+    @@artists << self
     @songs = []
+  end
+
+  def self.find_by_name(name)
+    @@artists.detect{|a| a.name == name}
   end
 
   def self.all
@@ -21,10 +24,11 @@ class Artist
 
   def add_song(song)
     @songs << song
-    song.artist = self
+    song.artist = self unless song.artist
   end
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
+
 end
