8268307cda1d0e90e875830138b08d7bde84dfb8
Sherry Martin <sm674@cornell.edu>
2018-09-30 19:04:50 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,16 @@
 require 'pry'
 
-# require_relative '../lib/concerns/findable'
-# require_relative '../lib/concerns/memorable'
+ require_relative '../lib/concerns/findable'
+ require_relative '../lib/concerns/memorable'
 # require_relative '../lib/concerns/paramable'
 
 require_relative '../lib/artist.rb'
 require_relative '../lib/song.rb'
+
+puts Artist.count
+puts Song.count
+puts Artist.reset_all
+puts Song.reset_all
+
+puts Artist.find_by_name(name)
+puts Song.find_by_name(name)
