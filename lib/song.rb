f9c7221987e50f6e838143205acb9eb88222abfb
Sherry Martin <sm674@cornell.edu>
2018-09-30 20:02:01 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,13 @@
 class Song
-  
+
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
-  include Paramable::InstanceMethods
   include Memorable::InstanceMethods
- 
- 
+  include Paramable::InstanceMethods
+
   attr_accessor :name
   attr_reader :artist
-  
+
   @@songs = []
 
   def self.all
